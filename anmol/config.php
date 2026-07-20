9f4b4df6f1f89a2532bb711aa210743872c36886
webcyber0 <hellowebcyber@gmail.com>
2026-07-20 07:05:31 +0530

    Delete anmol/config.php

--- anmol/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = [
-    'bot_token' => 'TELEGRAM_BOT_TOKEN',
-    'chat_id'   => 'CHAT_ID',
-    'website'   => 'https://yoursomin.com/',
-    'admin_username' => 'anish',
-    'admin_password' => 'exploits123'
-];
-?>
